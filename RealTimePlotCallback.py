5096abccef7272ebddf147962c4e45ac0092f28d
MichelDekiere <91069678+MichelDekiere@users.noreply.github.com>
2023-01-19 22:14:41 +0100

    updated comments

--- RealTimePlotCallback.py
+++ RealTimePlotCallback.py
@@ -2,8 +2,10 @@ import keras
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 
-class RealTimePlotCallback(keras.callbacks.Callback): # erft van keras.callbacks.Callback
-    """Zelf geschreven callback die de accuracy en loss in real-time afdrukt tijdens het trainen"""
+class RealTimePlotCallback(keras.callbacks.Callback): # inherits from keras.callbacks.Callback
+    """
+    Callback that plots training and validation accuracy and loss in real-time (after each epoch)
+    """
 
     def on_train_begin(self, logs):
         self._loss = []
@@ -12,19 +14,19 @@ class RealTimePlotCallback(keras.callbacks.Callback): # erft van keras.callbacks
         self._val_acc = []
 
     def on_epoch_end(self, epoch, logs={}):
-        clear_output() # vorige plot verwijderen
+        clear_output() # remove previous plot
         plt.clf()  # clear current figure
-        epochs = range(0, len(self._acc) + 1) # aantal epochs voor de x-as
+        epochs = range(0, len(self._acc) + 1) # amount of epochs as x-axis
 
-        # accuracy & loss van nieuwste epoch toevoegen aan de lists die gebruikt worden voor de plots
+        # add accuracy & loss of newest epoch to lists that will be used for the graphs
         self._acc.append(logs.get("accuracy"))
         self._val_acc.append(logs.get("val_accuracy"))
         self._loss.append(logs.get("loss"))
         self._val_loss.append(logs.get("val_loss"))
 
-        fig, (ax1, ax2) = plt.subplots(1, 2) # 2 plots naast elkaar zetten
+        fig, (ax1, ax2) = plt.subplots(1, 2) # 2 plots next to eachother
 
-        fig.set_size_inches(12, 3) # grootte van plots instellen
+        fig.set_size_inches(12, 3) # set size of plot
 
         # Accuracy plot
         ax1.plot(epochs, self._acc, "b-", label="Training accuracy")
@@ -42,9 +44,9 @@ class RealTimePlotCallback(keras.callbacks.Callback): # erft van keras.callbacks
         ax2.legend()
         ax2.grid()
 
-        plt.show() # tonen van plots
+        plt.show()
 
-        # printen van concrete loss & accuracy cijfers
+        # printen concrete loss & accuracy stats
         print(f"training loss: {self._loss[-1]}")
         print(f"validation loss: {self._val_loss[-1]}")
         print(f"training accuracy: {self._acc[-1]}")
